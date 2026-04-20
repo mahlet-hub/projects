9bde1f6e4286d80bd5d85d16161ac6be1eb7920e
Mahley Getu <mgetu01@slu.edu>
2026-04-20 10:12:17 -0500

    game.h

---
+++ src/model/game.h
@@ -0,0 +1,29 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <string>
+#include "board.h"
+
+class Game {
+private:
+    Board board;
+    std::string gameState;
+    std::string difficulty;
+
+public:
+    // Constructor
+    Game(std::string difficulty);
+
+    // Methods
+    void startGame();
+    void resetGame();
+    bool checkWin();  
+    void endGame(bool hitMine);
+
+    // Getters
+    std::string getGameState() const;
+    Board getBoard() const;
+    std::string getDifficulty() const;
+};
+
+#endif
